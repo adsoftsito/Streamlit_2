e54dbca3e863f01638049d9867c9d2038e3d42e1
Gerardo7567 <82042009+Gerardo7567@users.noreply.github.com>
2022-11-24 10:27:21 -0600

    Add files via upload

--- Netflix.py
+++ Netflix.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from os.path import splitext
 
 st.title('Streamlit - Netflix')
 sidebar = st.sidebar
@@ -20,14 +21,14 @@ if agree:
 @st.cache
 def load_data_byname(name):
     data = pd.read_csv(DATA_URL)
-    filtered_data_byname = data[data['name'].str.contains(name)]
+    filtered_data_byname = data[data['name'].str.upper().str.contains(name)]
 
     return filtered_data_byname
 
 myname = sidebar.text_input('Nombre de la pelicula: ')
 btnname = sidebar.button('Buscar por nombre')
 if (btnname):
-    filterbyname = load_data_byname(myname)
+    filterbyname = load_data_byname(myname.upper())
     count_row = filterbyname.shape[0]
     st.write(f"Peliculas Totales: {count_row}")
 
